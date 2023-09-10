8aeaa9b59b2c687a66b27b6de86cc72636ed46b7
Thomas Sullivan <sullivan.t@gmail.com>
2023-09-10 18:32:32 -0400

    Updated to return JSON on /balls and generally tinker with Warp routing.

--- service-ball/src/main.rs
+++ service-ball/src/main.rs
@@ -1,20 +1,36 @@
-use std::convert::Infallible;
-
 use warp::Filter;
+use warp::http::StatusCode;
+use warp::reply::json;
 
-use warp::{http::StatusCode};
 
 #[tokio::main]
 async fn main() {
-    let health_route = warp::path!("health")
-        .map(|| format!("I am healthy!"));
-    let routes = health_route
+
+    // GET /health
+    let route_health = warp::path!("health")
+        .map(|| warp::reply::with_status(format!("Hello from ball state."),  StatusCode::OK));
+    
+    // GET /balls
+    let route_ball = warp::path!("balls")
+        .and_then(|| handle_ball_state());
+
+    let routes = route_ball
+        .or(route_health)
         .with(warp::cors().allow_any_origin());
+
+    println!("Starting service-ball server.");
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
 
-// pub async fn handle_ball_state() -> Result<impl warp::Reply, Infallible> {
-//     let result = "hello".to_string();
+/// GET /balls
+/// 
+/// Will return the current state of all ball contact switches (75 in total).  This is done
+/// by querying the GPIO that is connected to the PISO Shift Registers.
+/// 
+/// TODO: Implement in hardware, for now this just returns 75 static bits that change only slightly
+///       to mimic a game in progress.
+pub async fn handle_ball_state() -> Result<impl warp::Reply, warp::Rejection> {
+    let ball_states = vec![0,0,0,0,1,1,1,1]; // Just a few balls to start
     
-//     Ok(warp::reply::json(&result))
-// }
+    Ok(warp::reply::with_status(json(&ball_states),  StatusCode::OK))
+}
