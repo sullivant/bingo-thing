546c559f65297da82acfd280466375756b6d0111
Thomas Sullivan <sullivan.t@gmail.com>
2023-09-12 17:31:07 -0400

    first connection to service-ball from service-ui.

--- service-ui/src/App.vue
+++ service-ui/src/App.vue
@@ -1,30 +1,33 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, reactive, ref } from 'vue'
+import { useFetch } from '@vueuse/core'
+import { useToggle } from '@vueuse/shared'
+
+
+function getPosts() {
+  fetch('http://localhost:8000/balls')
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    This is a simple header.
   </header>
 
   <main>
-    <TheWelcome />
+    This is main content.
+
+    <button @click="getPosts">Show Balls</button>
+
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
@@ -34,14 +37,5 @@ header {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
